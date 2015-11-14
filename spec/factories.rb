ff6457d88297e99e508df48b21417ae21c220226
Karianne Berg <karianne.berg@gmail.com>
2015-11-14 13:55:16 +0100

    Basic login

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,4 +1,10 @@
-FactoryGirl.define do  factory :meal do
+FactoryGirl.define do  factory :user do
+    
+  end
+  factory :model do
+    
+  end
+  factory :meal do
     title "MyString"
 description "MyText"
 picture_url "MyString"
