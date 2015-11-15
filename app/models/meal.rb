4fa5e52ce917eb8a6250ede658bd1ae65c463a8c
Karianne Berg <karianne.berg@gmail.com>
2015-11-15 12:28:34 +0100

    validate meal form

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -7,6 +7,11 @@ class Meal < ActiveRecord::Base
                            }
   belongs_to :user
 
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :time, presence: true
+  validates :price, presence: true, numericality: true
+
   # Validate the attached image is image/jpg, image/png, etc
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
